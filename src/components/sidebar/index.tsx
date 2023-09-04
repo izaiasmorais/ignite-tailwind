f5a0ede53804a405e83a04ba73bd6b5b3208da4f
Izaias Lima <izaiaslima356@gmail.com>
2023-09-04 13:53:39 -0300

    chore: complete user profile component and implement input with composition pattern

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -13,19 +13,19 @@ import {
 import { NavItem } from "./nav-item";
 import { UsedSpaceWidget } from "./used-space-widget";
 import { Profile } from "./profile";
+import { InputControl, InputPrefix, InputRoot } from "../input";
 
 export function Sidebar() {
 	return (
 		<aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
 			<Logo />
 
-			<div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-				<Search className="h-5 w-5 text-zinc-500 cursor-pointer hover:text-zinc-600" />
-				<input
-					placeholder="Search..."
-					className="flex-1 outline-none border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-				/>
-			</div>
+			<InputRoot>
+				<InputPrefix>
+					<Search className="w-5 h-5 text-zinc-500" />
+				</InputPrefix>
+				<InputControl placeholder="Search..." />
+			</InputRoot>
 
 			<nav className="space-y-0.5">
 				<NavItem title="Home" icon={Home} />
--- src/components/sidebar/profile.tsx
+++ src/components/sidebar/profile.tsx
@@ -1,7 +1,24 @@
+import { LogOut } from "lucide-react";
+
 export function Profile() {
 	return (
-		<div>
-			
+		<div className="flex items-center gap-3">
+			<img
+				src="https://github.com/izaiasmorais.png"
+				alt="Izaías Lima"
+				className="rounded-full w-10 h-10"
+			/>
+
+			<div className="truncate flex flex-1 flex-col ">
+				<span className="text-sm font-semibold text-zinc-700">Izaías Lima</span>
+				<span className="truncate text-sm text-zinc-500">
+					izaiaslima356@gmail.com
+				</span>
+			</div>
+
+			<button type="button" className="ml-auto p-2 hover:bg-zinc-50 rounded-md">
+				<LogOut className="w-5 h-5 text-zinc-500" />
+			</button>
 		</div>
-	)
+	);
 }
