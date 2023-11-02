4267f67455953b9de57beb8d81e0e7dbbb7dcfc4
Izaias Lima <izaiaslima356@gmail.com>
2023-11-02 13:06:09 -0300

    chore: start sidebar responsivity

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -17,7 +17,10 @@ import { InputControl, InputPrefix, InputRoot } from "../input";
 
 export function Sidebar() {
 	return (
-		<aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
+		<aside
+			className="border-b border-zinc-200 flex flex-col gap-6 p-4 fixed left-0 top-0 right-0
+			z-20 bg-white lg:right-auto lg:w-70 lg:border-r lg:px-5 lg:py-8 lg:relative "
+		>
 			<Logo />
 
 			<InputRoot>
